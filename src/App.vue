c5fe4846f073d384096f0a1870f620c9a9936d53
khemmatat <68525352+khemmatat@users.noreply.github.com>
2020-08-21 21:03:26 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
+<body>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Who do you like?" />
+    <hr />
+    <div class="row">
+      <div class="col-6">
+        <img alt="Vue logo" src="./assets/ire.png" />
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+        <img alt="Vue logo" src="./assets/fi.png" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum1" />
+      </div>
+    </div>
   </div>
+</body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import CSButton from "./components/CsButton";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CSButton,
+  },
+
+  data: function () {
+    return {
+      total1: 0,
+      total2: 0,
+    };
+  },
+
+  methods: {
+    sum(counterValue) {
+      this.total1 = counterValue;
+    },
+    sum1(counterValue) {
+      this.total2 = counterValue;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,7 +58,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
+}
+body {
+  background-image: url("assets/lol.jpg");
 }
 </style>
